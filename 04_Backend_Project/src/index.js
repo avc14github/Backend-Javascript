7ef35faa6c53d536829fd14c1c8a6884d5008adc
Aayush Jha <43124127+avc14github@users.noreply.github.com>
2024-08-29 10:45:11 +0000

    added initial code setup for Express backend

---
+++ 04_Backend_Project/src/index.js
@@ -0,0 +1,46 @@
+import "dotenv/config";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+connectDB()
+.then(() => {
+  app.on("error",(error) => {
+    console.log("ERR: ", error);
+    throw error;
+  })
+
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port ${process.env.PORT}`)
+  })
+})
+.catch((err) => {
+  console.log("MONGO DB Connection failed !!!", err)
+})
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+
+    app.get("/", (req, res) => {
+      res.send(`App is connected ${process.env.PORT}`);
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`app is connected on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw error;
+  }
+})();
+
+*/
+
